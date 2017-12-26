0e46e74cef4a00a490c32907402b48415a10ace8
Eduardo Patrocinio <epatro@gmail.com>
2017-12-26 14:14:17 -0200

    Fix balance validator

--- balanceValidator/src/balanceValidator.js
+++ balanceValidator/src/balanceValidator.js
@@ -1,15 +1,41 @@
 var Client = require('node-rest-client').Client;
-
 var client = new Client ();
 
+var INITIAL_BALANCE=100;
+
+var sum;
+var counter;
+var expected;
+
+function parseBalance(res, data) {
+  balance = data.balance;
+
+  console.log ("Balance: " + balance);
+  sum = sum + balance;
+}
+
+function checkSum(res){
+  res.setHeader('Content-Type', 'application/json');
+  if (sum == expected) {
+    res.send ("Sum and expected balances match: " + sum);
+  } else {
+    res.send ("PROBLEM!!! Sum: " + sum + " expected: " + expected);
+  }
+}
+
 function obtainBalance(res, account) {
-  var base_url = "http://balance-balance.svc.cluster.local/balance/";
+  var base_url = "http://balance-balance.microbank.svc.cluster.local/balance/";
 
   console.log ("Obtaining balance for account " + account);
-  url = base_url + accounts[i];
+  url = base_url + account;
+  console.log ("URL: " + url);
 
   client.get(url, function(data, response) {
-    console.log ("data: " + data);
+    parseBalance (res, data);
+    counter--;
+    if (counter == 0) {
+      checkSum (res);
+    }
   })
 }
 
@@ -17,6 +43,9 @@ function obtainBalances(res, data) {
   var accounts = data.accounts;
   console.log ("Obtainining balance... Number of accounts: " + accounts.length);
 
+  counter = accounts.length;
+  sum = 0;
+  expected = counter*INITIAL_BALANCE;
   for (var i = 0; i < accounts.length; i++) {
     obtainBalance(res, accounts[i]);
   }
@@ -27,8 +56,6 @@ function getAccounts (res) {
   client.get(url, function (data, response) {
     console.log ("data:" + data);
     obtainBalances (res, data);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(data);
   });
 }
 
