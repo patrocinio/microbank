e8c9e7d2a9043cbb8c0be693423c8876ac7f44e8
Eduardo Patrocinio <epatro@gmail.com>
2018-01-09 21:51:34 -0200

    Connection to redis is failing

--- src/display-accounts-job/1.0/server.js
+++ src/display-accounts-job/1.0/server.js
@@ -1,6 +1,12 @@
 var redis = require("redis");
 var client = redis.createClient("redis://microbank-account-system-redis");
 
+client.on("error", function (err) {
+    console.log("==> Error " + err);
+});
+
+//setInterval(client.ping(), 1000 * 60 * 30);
+
 function retrieveAccounts () {
     console.log ("Retrieving accounts");
     client.hkeys("accounts", function (err, replies) {
