363196b1b5b51a4a49f4881d3fd14073ba39df87
Eduardo Patrocinio <epatro@gmail.com>
2018-01-16 08:55:29 -0500

    Retry updating account when it returns 500

--- scripts/testTransfer.sh
+++ scripts/testTransfer.sh
@@ -1,1 +1,3 @@
-curl -d "from=john-account&to=mary-account&amount=10" -X POST http://50.22.210.194:30120/transfer
+PORT=31595
+
+curl -d "from=john-account&to=mary-account&amount=10" -X POST http://50.22.210.194:$PORT/transfer
