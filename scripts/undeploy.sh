c6374e7d679c9ffbd09ea7542d0dcceb96a6748b
Eduardo Patrocinio <epatro@gmail.com>
2018-01-19 11:42:03 -0500

    Fix account and account system interaction

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -3,13 +3,6 @@
 echo Deleting microbank
 helm delete --purge microbank
 
-for i in {1..100}
-do
-	account=account-$i
-	echo Deleting $account
-	helm delete --purge $account
-done
-
 ./switchNamespace.sh microbank-manager
 echo Deleting microbank-manager
 helm delete --purge microbank-manager
