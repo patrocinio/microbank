73850e84ba29dc58cf71dff56d11b510b8097596
Eduardo Patrocinio <epatro@gmail.com>
2018-02-03 09:29:20 -0500

    Fix undefined account number in transfer

---
+++ src/transfer-simulator/1.1/queue/channel.js
@@ -0,0 +1,41 @@
+const AMQP = require('amqplib/callback_api');
+
+const URL = 'amqp://guest:guest@microbank-rabbitmq:5672';
+
+module.exports = createQueueChannel;
+
+function createQueueChannel(queue, cb) {  
+	console.log ("Connecting to RabbitMQ. ");
+  AMQP.connect(URL, onceConnected);
+
+  function onceConnected(err, conn) {
+    console.log ("onceConnected");
+    if (err) {
+      cb(err);
+    } 
+    else {
+      console.log('connected');
+      conn.createChannel(onceChannelCreated);
+    }
+    function onceChannelCreated(err, channel) {
+      console.log ("onceChannelCreated");
+      if (err) {
+        cb(err);
+      }
+      else {
+        console.log('channel created');
+        channel.assertQueue(queue, {durable: true}, onceQueueCreated);
+      }
+
+      function onceQueueCreated(err) {
+        console.log ("onceQueueCreated");
+        if (err) {
+          cb(err);
+        }
+        else {
+          cb(null, channel, conn);
+        }
+      } // onceQueueCreated
+    } // onceChannelCreated
+  } // onceConnected
+} // createQueueChannel
