cff75fb4920a4f767237319a9920f3778ab2b749
Eduardo Patrocinio <epatro@gmail.com>
2018-02-05 12:56:02 -0500

    Implement queue mgr

---
+++ src/queue-mgr/1.0/queueMgr.js
@@ -0,0 +1,42 @@
+var Channel = require("./queue/channel.js");
+
+const UPDATE_QUEUE = "update";
+var queue = UPDATE_QUEUE;
+
+function check (req, res) {
+    console.log ("Waiting for a message");
+    Channel(queue, function(err, channel, conn) {  
+      if (err) {
+        console.error ("Error in estabishing connection to RabbitMQ... Retrying");
+        console.error (err.stack); 
+        setTimeout(check, 1e3, req, res);
+      }
+      else {
+        console.log('channel and queue created');
+        consume();
+      }
+
+      function consume() {
+        message = channel.get(queue, {}, onConsume);
+
+        function onConsume(err, msg) {
+          if (err) {
+            console.warn(err.message);
+          }
+          else if (msg) {
+            console.log ("Found message " + msg.content);
+            res.status(400).send ("There are pending messages");
+          }
+          else {
+            // console.log('no message, waiting...');
+            res.send ("No messages found");
+          } // else
+        } // onConsume
+      } // consume
+    }); // Channel
+  } // checkEmptyChannel
+
+
+module.exports = {
+  check: check
+}
---
+++ src/transfer/1.4/transfer.js
@@ -0,0 +1,38 @@
+var queue = require('./queue/queue');
+const UPDATE_QUEUE = "update";
+
+var logger = require('./logger/logger');
+
+function updateAccount(res, account, amount) {
+  message = { account : account, amount: amount };
+
+  console.log ("Sending " + amount + " to " + account); 
+  logger.logMessage ("Sending " + amount + " to " + account); 
+
+  queue.sendMessage (UPDATE_QUEUE, JSON.stringify(message)); 
+}
+
+function transfer (res, from, to, amount) {
+
+  console.log ("===> Transfering from account " + from + " to " + to + 
+    " amount " + amount);
+  logger.logMessage ("Transfering from account " + from + " to " + to + 
+    " amount " + amount);
+
+  updateAccount (res, from, amount.toString());
+  var withdrawal = -amount;
+  updateAccount (res, to, withdrawal.toString());
+  res.send("Done!");
+}
+
+
+module.exports = {
+    transfer: function(req, res) {
+      console.log ("Body: ");
+      console.log (req.body);
+      from = req.body.from;
+      to = req.body.to;
+      amount = req.body.amount;
+      transfer (res, from, to, amount);
+    }
+}
