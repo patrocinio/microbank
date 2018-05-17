82b9676f00db7dbafc881cb4d08fd8c12fab8da6
Eduardo Patrocinio <epatro@gmail.com>
2018-05-17 08:37:54 -0300

    Add createVolumes

--- scripts/createVolumes.sh
+++ scripts/createVolumes.sh
@@ -1,5 +1,5 @@
-FILE_SERVER=fsf-wdc0401e-fz.service.softlayer.com
-FILE_PATH=/IBM02SV625675_6/data01
+FILE_SERVER=fsf-wdc0701b-fz.adn.networklayer.com
+FILE_PATH=/IBM02SV625675_7/data01
 TEMP_FILE=/tmp/volume.yaml
 
 # args: 
@@ -20,12 +20,8 @@ function create_pv () {
 	done
 }
 
-#create_pv 'rwo' 1 20
-#create_pv 'rwx' 21 40
-#create_pv 'rwx-large' 41 50
-#create_pv 'rwo-large' 51 60
+create_pv 'rwo' 1 20
+create_pv 'rwx' 21 40
+create_pv 'rwx-large' 41 50
+create_pv 'rwo-large' 51 60
 
-create_pv 'rwo' 61 80
-create_pv 'rwx' 81 100
-create_pv 'rwx-large' 101 110
-create_pv 'rwo-large' 111 120
