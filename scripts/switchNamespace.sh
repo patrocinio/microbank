21ed5d85d84aef66c3b49bcfed06d964ce381810
Eduardo Patrocinio <epatro@gmail.com>
2018-05-22 21:32:19 -0300

    Fix for ICP 2.1.0.2

--- scripts/switchNamespace.sh
+++ scripts/switchNamespace.sh
@@ -1,14 +1,9 @@
-# ICP
 CONTEXT=mycluster.icp-context
 USER=admin
-
-# minikube
-#CONTEXT=minikube
-#USER=minikube
-
 NAMESPACE=$1
 
+echo Switching to namespace $NAMESPACE
 kubectl config set-context $CONTEXT --user=$USER --namespace=$NAMESPACE
-
-echo Namespace set to $NAMESPACE
+kubectl config use-context $CONTEXT
+kubectl config get-contexts
 
--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -1,13 +1,13 @@
 
 ./switchNamespace.sh 
 echo Deleting microbank
-helm delete --purge microbank
+helm delete --purge microbank --tls
 
 ./switchNamespace.sh microbank-manager
 echo Deleting microbank-manager
-helm delete --purge microbank-manager
+helm delete --purge microbank-manager --tls
 
 ./switchNamespace.sh microbank-simulator
 echo Deleting microbank-simulator
-helm delete --purge microbank-simulator
+helm delete --purge microbank-simulator --tls
 
