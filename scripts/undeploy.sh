2ddca132d08dbd171124bded4ca814c35d0306b6
Eduardo Patrocinio <epatro@amazon.com>
2022-08-03 13:05:39 -0400

    Remove old flag from helm delete

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -4,13 +4,13 @@ HELM_OPTIONS=
 
 ./switchNamespace.sh microbank
 echo Deleting microbank
-helm delete --purge microbank $HELM_OPTIONS
+helm delete microbank $HELM_OPTIONS
 
 ./switchNamespace.sh microbank-manager
 echo Deleting microbank-manager
-helm delete --purge microbank-manager $HELM_OPTIONS
+helm delete microbank-manager $HELM_OPTIONS
 
 ./switchNamespace.sh microbank-simulator
 echo Deleting microbank-simulator
-helm delete --purge microbank-simulator $HELM_OPTIONS
+helm delete microbank-simulator $HELM_OPTIONS
 
