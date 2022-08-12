215429290adb8843d93cd7b13e5603c57806e7f0
Eduardo Patrocinio <epatro@amazon.com>
2022-08-12 10:12:23 -0400

    Work on account MySQL

---
+++ src/account/2.6/mysql/mysql.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+function connect () {
+  console.log ("Connecting to DB...");
+
+  connection = mysql.createConnection({
+    host     : 'microbank-account-mysql',
+    user     : 'root',
+    password : 'admin',
+    database : 'account'
+  });
+
+  connection.connect();
+  console.log ("Connected to DB");
+}
+
+module.exports = {
+	connect : connect
+}
